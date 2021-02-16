56da262c47ee0fa9a6d161845fb6e9bcaa8704b3
Ben Chartrand <bcnzer@users.noreply.github.com>
2021-02-16 20:08:16 +1300

    build the dog quest

--- main.ts
+++ main.ts
@@ -1,75 +1,174 @@
 namespace SpriteKind {
     export const Building = SpriteKind.create()
+    export const Boy = SpriteKind.create()
+    export const Dog = SpriteKind.create()
 }
+function gameIntro () {
+    game.splash("Welcome to", "AMELIE VALLEY")
+    characterName = game.askForString("What is your name?")
+}
+function worldSetup () {
+    myCharactersHouse = sprites.create(assets.image`mySplashscreen`, SpriteKind.Building)
+    myCharactersHouse.setPosition(80, 35)
+    myCharactersField = sprites.create(assets.image`field`, SpriteKind.Player)
+    myCharactersField.setPosition(133, 40)
+    myMainCharacter = sprites.create(img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f f . . . . . 
+        . . . . f 1 5 2 5 1 6 f . . . . 
+        . . . f 1 6 6 6 6 6 1 6 f . . . 
+        . . . f 6 6 f f f f 6 1 f . . . 
+        . . . f 6 f f d d f f 6 f . . . 
+        . . f 6 f d f d d f d f 6 f . . 
+        . . f 6 f d 3 d d 3 d f 6 f . . 
+        . . f 6 6 f d d d d f 6 6 f . . 
+        . f 6 6 f 3 f f f f 3 f 6 6 f . 
+        . . f f d 3 5 3 3 5 3 d f f . . 
+        . . f d d f 3 5 5 3 f d d f . . 
+        . . . f f 3 3 3 3 3 3 f f . . . 
+        . . . f 3 3 5 3 3 5 3 3 f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    scene.cameraFollowSprite(myMainCharacter)
+    controller.moveSprite(myMainCharacter)
+}
+function questBoyAndLostDog () {
+    characterBoy = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Boy)
+    characterBoy.setPosition(100, 100)
+    characterBoy.setFlag(SpriteFlag.DestroyOnWall, true)
+    characterLostDog = sprites.create(img`
+        . . 4 4 4 . . . . 4 4 4 . . . . 
+        . 4 5 5 5 e . . e 5 5 5 4 . . . 
+        4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+        4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+        e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+        . e e 5 5 5 5 5 5 5 5 e e . . . 
+        . . e 5 f 5 5 5 5 f 5 e . . . . 
+        . . f 5 5 5 4 4 5 5 5 f . . f f 
+        . . f 4 5 5 f f 5 5 6 f . f 5 f 
+        . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+        . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+        . . . f 5 5 5 5 5 4 5 5 f f . . 
+        . . . f 5 f f f 5 f f 5 f . . . 
+        . . . f f . . f f . . f f . . . 
+        `, SpriteKind.Dog)
+    characterLostDog.setPosition(300, 100)
+}
+sprites.onOverlap(SpriteKind.Boy, SpriteKind.Dog, function (sprite, otherSprite) {
+    game.setDialogCursor(img`
+        . . 4 4 4 . . . . 4 4 4 . . . . 
+        . 4 5 5 5 e . . e 5 5 5 4 . . . 
+        4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+        4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+        e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+        . e e 5 5 5 5 5 5 5 5 e e . . . 
+        . . e 5 f 5 5 5 5 f 5 e . . . . 
+        . . f 5 5 5 4 4 5 5 5 f . . f f 
+        . . f 4 5 5 f f 5 5 6 f . f 5 f 
+        . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+        . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+        . . . f 5 5 5 5 5 4 5 5 f f . . 
+        . . . f 5 f f f 5 f f 5 f . . . 
+        . . . f f . . f f . . f f . . . 
+        `)
+    game.setDialogFrame(img`
+        ..bbbbbbbbbbbbbbbbbbbb..
+        .bd111111111111111111db.
+        bd1dbbbbbbbbbbbbbbbbd1db
+        b1dbbbbbbbbbbbbbbbbbbd1b
+        b1bd1111111111111111db1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1b111111111111111111b1b
+        b1bd1111111111111111db1b
+        bd1bbbbbbbbbbbbbbbbbb1db
+        bbd111111111111111111dbb
+        .bbbbbbbbbbbbbbbbbbbbbb.
+        ..bbbbbbbbbbbbbbbbbbbb..
+        `)
+    game.showLongText("Doggy made it back to his owner", DialogLayout.Bottom)
+    otherSprite.destroy()
+    listQuests[0] = "dog found"
+})
+let listQuests: string[] = []
+let characterLostDog: Sprite = null
+let characterBoy: Sprite = null
+let myMainCharacter: Sprite = null
+let myCharactersField: Sprite = null
+let myCharactersHouse: Sprite = null
+let characterName = ""
 tiles.setTilemap(tilemap`levelAmelieValley`)
-let myCharactersField = sprites.create(assets.image`field`, SpriteKind.Player)
-let myCharactersHouse = sprites.create(img`
-    ....................e2e22e2e....................
-    .................222eee22e2e222.................
-    ..............222e22e2e22eee22e222..............
-    ...........e22e22eeee2e22e2eeee22e22e...........
-    ........eeee22e22e22e2e22e2e22e22e22eeee........
-    .....222e22e22eeee22e2e22e2e22eeee22e22e222.....
-    ...22eeee22e22e22e22eee22eee22e22e22e22eeee22...
-    4cc22e22e22eeee22e22e2e22e2e22e22eeee22e22e22cc4
-    6c6eee22e22e22e22e22e2e22e2e22e22e22e22e22eee6c6
-    46622e22eeee22e22eeee2e22e2eeee22e22eeee22e22664
-    46622e22e22e22eeee22e2e22e2e22eeee22e22e22e22664
-    4cc22eeee22e22e22e22eee22eee22e22e22e22eeee22cc4
-    6c622e22e22eeee22e22e2e22e2e22e22eeee22e22e226c6
-    466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
-    46622e22eeee22e22e22e2e22e2e22e22e22eeee22e22664
-    4cc22e22e22e22e22eeee2e22e2eeee22e22e22e22e22cc4
-    6c622eeee22e22eeee22eee22eee22eeee22e22eeee226c6
-    46622e22e22eeee22e22e2e22e2e22e22eeee22e22e22664
-    466eee22e22e22e22e22e2e22e2e22e22e22e22e22eee664
-    4cc22e22eeee22e22e22e2e22e2e22e22e22eeee22e22cc4
-    6c622e22e22e22e22e22eee22eee22e22e22e22e22e226c6
-    46622eeee22e22e22eeecc6666cceee22e22e22eeee22664
-    46622e22e22e22eeecc6666666666cceee22e22e22e22664
-    4cceee22e22eeecc66666cccccc66666cceee22e22eeecc4
-    6c622e22eeecc66666cc64444446cc66666cceee22e226c6
-    46622e22cc66666cc64444444444446cc66666cc22e22664
-    46622cc6666ccc64444444444444444446ccc6666cc22664
-    4ccc6666ccc6444bcc666666666666ccb4446ccc6666ccc4
-    cccccccc6666666cb44444444444444bc6666666cccccccc
-    64444444444446c444444444444444444c64444444444446
-    66cb444444444cb411111111111111114bc444444444bc66
-    666cccccccccccd166666666666666661dccccccccccc666
-    6666444444444c116eeeeeeeeeeeeee611c4444444446666
-    666e2222222e4c16e4e44e44e44e44ee61c4e2222222e666
-    666eeeeeeeee4c16e4e44e44e44e44ee61c4eeeeeeeee666
-    666eddddddde4c66f4e4effffffe44ee66c4eddddddde666
-    666edffdffde4c66f4effffffffff4ee66c4edffdffde666
-    666edccdccde4c66f4effffffffffeee66c4edccdccde666
-    666eddddddde4c66f4eeeeeeeeeeeeee66c4eddddddde666
-    c66edffdffde4c66e4e44e44e44e44ee66c4edffdffde66c
-    c66edccdccde4c66e4e44e44e44e44ee66c4edccdccde66c
-    cc66666666664c66e4e44e44e44feeee66c46666666666cc
-    .c66444444444c66e4e44e44e44ffffe66c44444444466c.
-    ..c64eee4eee4c66f4e44e44e44f44fe66c4eee4eee46c..
-    ...c4eee4eee4c66f4e44e44e44effee66c4eee4eee4c...
-    ....644444444c66f4e44e44e44e44ee66c444444446....
-    .....64eee444c66f4e44e44e44e44ee66c444eee46.....
-    ......6ccc666c66e4e44e44e44e44ee66c666ccc6......
-    `, SpriteKind.Building)
-let myMainCharacter = sprites.create(img`
-    . . . . . . 5 . 5 . . . . . . . 
-    . . . . . f 5 5 5 f f . . . . . 
-    . . . . f 1 5 2 5 1 6 f . . . . 
-    . . . f 1 6 6 6 6 6 1 6 f . . . 
-    . . . f 6 6 f f f f 6 1 f . . . 
-    . . . f 6 f f d d f f 6 f . . . 
-    . . f 6 f d f d d f d f 6 f . . 
-    . . f 6 f d 3 d d 3 d f 6 f . . 
-    . . f 6 6 f d d d d f 6 6 f . . 
-    . f 6 6 f 3 f f f f 3 f 6 6 f . 
-    . . f f d 3 5 3 3 5 3 d f f . . 
-    . . f d d f 3 5 5 3 f d d f . . 
-    . . . f f 3 3 3 3 3 3 f f . . . 
-    . . . f 3 3 5 3 3 5 3 3 f . . . 
-    . . . f f f f f f f f f f . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-scene.cameraFollowSprite(myMainCharacter)
-controller.moveSprite(myMainCharacter)
+worldSetup()
+questBoyAndLostDog()
+game.onUpdate(function () {
+    if (myMainCharacter.overlapsWith(characterLostDog)) {
+        characterLostDog.say("Woof woof")
+        characterLostDog.follow(characterBoy)
+    }
+})
+game.onUpdate(function () {
+    if (myMainCharacter.overlapsWith(characterBoy)) {
+        game.setDialogFrame(img`
+            ..99999999999999999999..
+            .9966666666666666666699.
+            996661111111111111166699
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            966611111111111111116669
+            996661111111111111166699
+            .9966666666666666666699.
+            ..99999999999999999999..
+            `)
+        if (listQuests[0] == "dog found") {
+            characterBoy.vx = -50
+            game.showLongText("Thank you so much!", DialogLayout.Bottom)
+        } else {
+            game.showLongText("Please help me find my dog", DialogLayout.Bottom)
+        }
+        characterBoy.y += 20
+    }
+})
